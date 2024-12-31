8a1b3af5de81a7e899bdfd4d1e3d73ef19f7ac8f
DeveloperYuri <developeryuri2@gmail.com>
2024-12-31 14:14:21 +0700

    change dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function AdminDashboard(Request $request){
-        return view('admin.admin_dashboard');
+        return view('admin.index');
     }
 }
